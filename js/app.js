b041020dac3425d282a12c1136b87b0542c00cfd
Carlos Ramirez <karlossander92@gmail.com>
2022-03-09 20:39:19 -0500

    Primer Commit

---
+++ js/app.js
@@ -0,0 +1,54 @@
+//Variables y Selectores
+const formulario = document.querySelector('#agregar-gasto');
+const gastoListado = document.querySelector('#gastos ul');
+
+//Eventos
+eventListeners();
+function eventListeners(){
+    document.addEventListener('DOMContentLoaded', pregutarPresupuesto);
+
+    formulario.addEventListener('submit', agregarGasto);
+}
+
+//Clases
+class Presupuesto{
+    constructor(presupuesto){
+        this.presupuesto = Number(presupuesto);
+        this.restante = Number(presupuesto);
+        this.gasto = [];
+    }
+}
+
+class UI{
+    insertarPresupuesto ( cantidad ){
+        //Extrallendo el valor
+        const { presupuesto, restante } = cantidad;
+
+        //Agregar al HTML
+        document.querySelector('#total').textContent = presupuesto;
+        document.querySelector('#restante').textContent = restante;
+    }
+}
+
+
+//Instanciar
+const ui = new UI();
+let presupuesto;
+
+//Funciones
+
+function pregutarPresupuesto(){
+    const presupuestoUsuario = prompt('¿Cual es tu presupuesto?');
+
+    // console.log( Number( presupuestoUsuario ) );
+
+    if(presupuestoUsuario === '' || presupuestoUsuario === null || isNaN(presupuestoUsuario) || presupuestoUsuario <= 0){
+        window.location.reload();
+    }
+
+    //Presupuesto valido
+    presupuesto = new Presupuesto(presupuestoUsuario);
+    console.log(presupuesto);
+
+    ui.insertarPresupuesto(presupuesto);
+}
